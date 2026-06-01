84d64feb97bc1cac1c6cd9ec0b8302a279ad1eea
hkeyd-124 <nhatkycuavuong2@gmail.com>
2026-06-01 15:58:47 +0700

    Update testMint.js

--- api/testMint.js
+++ api/testMint.js
@@ -3,39 +3,20 @@ export default async function handler(
   res
 ){
 
-  const response =
-    await fetch(
-      `${req.headers.origin}/api/mintCertificate`,
-      {
-        method:"POST",
-        headers:{
-          "Content-Type":
-            "application/json"
-        },
-        body:JSON.stringify({
+  return res.status(200).json({
 
-          uid:
-            "uid_5a7638f2-9e11-47e9-bfd9-478e69c0ab4e",
+    uid:
+      "uid_5a7638f2-9e11-47e9-bfd9-478e69c0ab4e",
 
-          lessonId:
-            "organic_1",
+    lessonId:
+      "organic_1",
 
-          wallet:
-            "0x3c5945b76525e83650f3faa2923d6fa8ecf1ee81",
+    wallet:
+      "0x3c5945b76525e83650f3faa2923d6fa8ecf1ee81",
 
-          metadataURI:
-            "ipfs://QmTest"
+    metadataURI:
+      "ipfs://QmTest"
 
-        })
-
-      }
-    );
-
-  const data =
-    await response.json();
-
-  return res
-    .status(200)
-    .json(data);
+  });
 
 }
