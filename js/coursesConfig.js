9e29a29d7b003d2a8f5070663433da13cc4a1252
hkeyd-124 <nhatkycuavuong2@gmail.com>
2026-06-07 22:53:34 +0700

    Create coursesConfig.js

---
+++ js/coursesConfig.js
@@ -0,0 +1,85 @@
+window.COURSE_CONFIG = [
+
+    /* =========================
+       TOPIC 1
+    ========================= */
+
+    {
+        id: "hydrocarbon",
+
+        title: "🧬 Hữu cơ & Hydrocarbon",
+
+        bossLesson: "hydrocarbon_master",
+
+        lessons: [
+
+            {
+                id: "organic_1",
+                name: "Hợp chất hữu cơ",
+                total: 15
+            },
+
+            {
+                id: "organic_2",
+                name: "Alkane",
+                total: 15
+            },
+
+            {
+                id: "organic_3",
+                name: "Alkene",
+                total: 15
+            },
+
+            {
+                id: "organic_4",
+                name: "Nhiên liệu",
+                total: 15
+            }
+
+        ]
+    },
+
+
+
+    /* =========================
+       TOPIC 2
+    ========================= */
+
+    {
+        id: "alcohol",
+
+        title: "🍺 Alcohol",
+
+        bossLesson: "alcohol_master",
+
+        lessons: [
+
+            {
+                id: "alcohol_1",
+                name: "Khái niệm Alcohol",
+                total: 15
+            },
+
+            {
+                id: "alcohol_2",
+                name: "Danh pháp Alcohol",
+                total: 15
+            },
+
+            {
+                id: "alcohol_3",
+                name: "Tính chất vật lý",
+                total: 15
+            },
+
+            {
+                id: "alcohol_4",
+                name: "Tính chất hóa học",
+                total: 15
+            }
+
+        ]
+    }
+
+];
