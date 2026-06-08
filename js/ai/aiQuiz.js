5ce00a829b13b99d6cc1e3b89e197c33d6028eed
hkeyd-124 <nhatkycuavuong2@gmail.com>
2026-06-08 14:13:16 +0700

    Update aiQuiz.js

--- js/ai/aiQuiz.js
+++ js/ai/aiQuiz.js
@@ -1,3 +1,18 @@
+window.openAIQuiz = function(){
+
+  document.getElementById(
+    "aiHomeView"
+  ).style.display = "none";
+
+  document.getElementById(
+    "aiTutorView"
+  ).style.display = "none";
+
+  document.getElementById(
+    "aiQuizView"
+  ).style.display = "block";
+};
+
 window.backToAIHome = function(){
 
   document.getElementById(
