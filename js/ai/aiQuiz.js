0c3230ee45957abbf7c18eb240d0a634d13920fe
hkeyd-124 <nhatkycuavuong2@gmail.com>
2026-06-08 14:18:51 +0700

    Update aiQuiz.js

--- js/ai/aiQuiz.js
+++ js/ai/aiQuiz.js
@@ -27,3 +27,80 @@ window.backToAIHome = function(){
     "aiHomeView"
   ).style.display = "block";
 };
+document.addEventListener(
+
+  "click",
+
+  e=>{
+
+    if(
+      e.target.id ===
+      "generateQuizBtn"
+    ){
+
+      document.getElementById(
+        "quizResult"
+      ).style.display =
+        "block";
+
+      document.getElementById(
+        "quizQuestion"
+      ).innerText =
+        "What is the chemical symbol of Water?";
+
+      document.getElementById(
+        "quizAnswers"
+      ).innerHTML = `
+
+        <button
+          style="
+          width:100%;
+          margin-bottom:10px;
+          padding:12px;
+          border-radius:12px;
+          border:1px solid #ddd;
+          cursor:pointer;
+          ">
+          A. H₂O
+        </button>
+
+        <button
+          style="
+          width:100%;
+          margin-bottom:10px;
+          padding:12px;
+          border-radius:12px;
+          border:1px solid #ddd;
+          cursor:pointer;
+          ">
+          B. CO₂
+        </button>
+
+        <button
+          style="
+          width:100%;
+          margin-bottom:10px;
+          padding:12px;
+          border-radius:12px;
+          border:1px solid #ddd;
+          cursor:pointer;
+          ">
+          C. NaCl
+        </button>
+
+        <button
+          style="
+          width:100%;
+          padding:12px;
+          border-radius:12px;
+          border:1px solid #ddd;
+          cursor:pointer;
+          ">
+          D. O₂
+        </button>
+
+      `;
+    }
+
+  }
+);
