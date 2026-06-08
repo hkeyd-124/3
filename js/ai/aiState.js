5e0850359e136101265b689a01cb443ddc9372d3
hkeyd-124 <nhatkycuavuong2@gmail.com>
2026-06-08 16:35:21 +0700

    Update aiState.js

--- js/ai/aiState.js
+++ js/ai/aiState.js
@@ -11,3 +11,23 @@ window.AI_STATE = {
   quizAnswered: false
 
 };
+
+try{
+
+  const savedMessages =
+
+    localStorage.getItem(
+      "hackchem_ai_messages"
+    );
+
+  if(savedMessages){
+
+    AI_STATE.messages =
+      JSON.parse(savedMessages);
+  }
+
+}catch(err){
+
+  console.error(err);
+
+}
